676883217946a478ad2b325eed1ba5cbd4f3dd94
SusgUY446 <129160115+SusgUY446@users.noreply.github.com>
2023-10-09 19:06:47 +0200

    Update index.php
    added php back (Why was it missing?)

--- code/index.php
+++ code/index.php
@@ -36,7 +36,26 @@
   </head>
   <body>
     <?php
-    
+    if(isset($_POST["submit"])){
+      require("mysql.php");
+      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
+      $stmt->bindParam(":user", $_POST["username"]);
+      $stmt->execute();
+      $count = $stmt->rowCount();
+      if($count == 1){
+        //Username ist frei
+        $row = $stmt->fetch();
+        if(password_verify($_POST["pw"], $row["PASSWORD"])){
+          session_start();
+          $_SESSION["username"] = $row["USERNAME"];
+          header("Location: private.php");
+        } else {
+          echo "Der Login ist fehlgeschlagen";
+        }
+      } else {
+        echo "Der Login ist fehlgeschlagen";
+      }
+    }
     ?>
     <div class="login-container">
       <h1>Anmelden</h1>
