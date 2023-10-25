ed8347e5c59d13eb97bf9f82774d49927899c47c
SusgUY446 <129160115+SusgUY446@users.noreply.github.com>
2023-10-25 19:21:54 +0200

    Update private.php

--- code/private.php
+++ code/private.php
@@ -9,8 +9,11 @@ $username = $_SESSION["username"];
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="loader.css">
     <meta charset="utf-8">
     <title>Your Personal Site</title>
+
     <style>
       body {
         background-color: lightblue;
@@ -19,6 +22,9 @@ $username = $_SESSION["username"];
         align-items: center;
         height: 100vh;
         margin: 0;
+      }
+      #settings {
+
       }
       .container {
         align-self: flex-start;
@@ -29,87 +35,122 @@ $username = $_SESSION["username"];
         text-align: center;
       }
 
-      .loader {
-        position: fixed;
-         top: 0;
-        left: 0;
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background: #333333;
-        transition: opacity 2s, visibility 2s;
-    }
-
-    .loader--hidden {
-        opacity: 0;
-        visibility: hidden;
-    }
-
-    .loader::after {
-        content: "";
-        width: 75px;
-        height: 75px;
-        border: 15px solid #dddddd;
-        border-top-color: blue;
-        border-radius: 50%;
-        animation: loading 2s ease infinite;
-    }
-
-    @keyframes loading {
-        from {
-            transform: rotate(0turn);
-        }
-        to {
-            transform: rotate(1turn);
-        }
-    }
 
     </style>
-    <script>
-      window.addEventListener("load", () => {
-          document.querySelector(".loader").classList.add("loader--hidden");
+    <style>
+      
+.sidenav {
+  height: 100%; 
+  width: 0; 
+  position: fixed; 
+  z-index: 1; 
+  top: 0; 
+  left: 0;
+  background-color: #111; /
+  overflow-x: hidden; 
+  padding-top: 60px; 
+  transition: 0.5s; 
+}
 
-          document.querySelector(".loader").addEventListener("transitionend", () => {
-              document.body.removeChild(querySelector(".loader"))
-            });
+.sidenav a {
+  opacity: 0;
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
 
-      });
-    </script>
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+
+#main {
+  transition: margin-left .5s;
+  padding: 20px;
+}
+
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+} 
+    </style>
+    <script type="text/javascript" src="loader.js"></script>
     <script>
         
         const imageFiles = [
-            'dog.jpg',
-            'dog2.jpg',
-            'dog3.jpg',
+            'baldur.jpg',
+            'baldur2.jpg',
+            'baldur3.jpg',
             
         ];
 
-        // Function to set a random image from the array
+        
         function setRandomImage() {
             const randomIndex = Math.floor(Math.random() * imageFiles.length);
             const randomImage = imageFiles[randomIndex];
             const imgElement = document.querySelector('.random-img');
-            imgElement.src = `/personal-site/images/${randomImage}`; // Update the src attribute
+            imgElement.src = `/personal-site/images/${randomImage}`; 
         }
 
-        // Call the function to set a random image when the page loads
+        
        
     </script>
+    <script>
+    
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById(".sidenav a").style.opacity = "1";
+  }
 
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById(".sidenav a").style.opacity = "0";
+  } 
+    </script>
 
   </head>
   <body>
     
-    <div class="loader"></div>
+  <div class="loader"></div>
+  <div id="main">
     <div class="container">
       <h1>Hallo <?php echo $username?></h1>
-      <img src="" alt="random image of my dog" width="950px" height="950px" class="random-img">
-      <script>
-        setRandomImage();
-      </script>
+      <!--<img src="" alt="random image of my dog" width="900px" height="900px" class="random-img">-->
+      <!--
+        <script>
+          setRandomImage();
+        </script>
+    -->
     </div>
+  </div>
+  <!--Settings-->
+    <div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <a href="#">About</a>
+      <a href="#">Services</a>
+      <a href="#">Clients</a>
+      <a href="#">Contact</a>
+    </div>
+
+    
+
+      
+
+    
 
   </body>
 </html>
