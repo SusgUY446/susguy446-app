d6c11e89a563bd35fffe2c29e86b76841cbde703
SusgUY446 <129160115+SusgUY446@users.noreply.github.com>
2023-10-26 18:52:31 +0200

    Delete code/loader.js

--- code/loader.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-    document.querySelector(".loader").classList.add("loader--hidden");
-
-    document.querySelector(".loader").addEventListener("transitionend", () => {
-        document.body.removeChild(querySelector(".loader"))
-      });
-
-});
