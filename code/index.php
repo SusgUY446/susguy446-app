e43f053e30e0f29e216eff9d406ce8fbc4180731
SusgUY446 <129160115+SusgUY446@users.noreply.github.com>
2023-10-28 19:44:27 +0200

    Update index.php

--- code/index.php
+++ code/index.php
@@ -5,18 +5,10 @@
     <title>Login</title>
     <link rel="stylesheet" href="cnd/css/loader.css">
     <link rel="stylesheet" href="cnd/css/container.css">
+    <link rel="stylesheet" href="cnd/css/background.css">
+    <link rel="stylesheet" href="cnd/css/button.css">
+    <link rel="stylesheet" href="cnd/css/login-form.css">
     <style>
-      body {
-        background-color: lightblue;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        margin: 0;
-      }
-
-
-
       .login-form {
         margin-top: 10px;
       }
@@ -25,10 +17,9 @@
         margin-bottom: 10px;
       }
 
-      .button {
-        margin-top: 10px;
-      }
+
     </style>
+    <script src="cnd/js/loader.js"></script>
   </head>
   <body>
   <?php
@@ -36,38 +27,40 @@
 
     if(isset($_POST["submit"])){
       require("mysql.php");
-      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
+      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user");
       $stmt->bindParam(":user", $_POST["username"]);
       $stmt->execute();
       $count = $stmt->rowCount();
       if($count == 1){
-        //Username ist frei
+        
         $row = $stmt->fetch();
         if(password_verify($_POST["pw"], $row["PASSWORD"])){
           session_start();
           $_SESSION["username"] = $row["USERNAME"];
           header("Location: private.php");
         } else {
-          echo "Der Login ist fehlgeschlagen";
+          echo "The login failed";
         }
       } else {
-        echo "Der Login ist fehlgeschlagen";
+        echo "The login failed";
       }
     }
      ?>
+    <div class="loader"></div>
+    
     <div class="container">
-      <h1>Anmelden</h1>
+      <h1>Login</h1>
       <form action="index.php" method="post" class="form">
         <div class="input-field">
           <input type="text" name="username" placeholder="Username" class="username-input" required>
         </div>
         <div class="input-field">
-          <input type="password" name="pw" placeholder="Passwort" class="password-input" required>
+          <input type="password" name="pw" placeholder="Password" class="password-input" required>
         </div>
-        <button type="submit" name="submit" class="button">Einloggen</button>
+        <button type="submit" name="submit" class="login-button">Login</button>
       </form>
     </br>
-      <a href="register.php" class="button">Noch keinen Account</a>
+      <a href="register.php" class="register">Register</a>
       
     </div>
   </body>
