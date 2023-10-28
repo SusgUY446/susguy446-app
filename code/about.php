c30214661a9140771d6b3380f0696c41f7e747db
SusgUY446 <129160115+SusgUY446@users.noreply.github.com>
2023-10-28 19:43:24 +0200

    Update about.php

--- code/about.php
+++ code/about.php
@@ -7,101 +7,15 @@
     <title>Sidebar</title>
     <link rel="stylesheet" href="cnd/css/loader.css">
     <link rel="stylesheet" href="cnd/css/container.css">
-    <style>
-    body {
-        background-color: lightblue;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        margin: 0;
-    }
-    
-    
-         /* The side navigation menu */
-    .sidenav {
-        height: 100%; /* 100% Full-height */
-        width: 0; /* 0 width - change this with JavaScript */
-        position: fixed; /* Stay in place */
-        z-index: 1; /* Stay on top */
-        top: 0; /* Stay at the top */
-        left: 0;
-        background-color: #111; /* Black*/
-        overflow-x: hidden; /* Disable horizontal scroll */
-        padding-top: 60px; /* Place content 60px from the top */
-        transition: 0.5s; /* 0.5 second transition effect to slide in the sidenav */
-    }
+    <link rel="stylesheet" href="cnd/css/background.css">
 
-    /* The navigation menu links */
-    .sidenav a {
-        padding: 8px 8px 8px 32px;
-        text-decoration: none;
-        font-size: 25px;
-        color: #818181;
-        display: block;
-        transition: 0.3s;
-    }
-
-    /* When you mouse over the navigation links, change their color */
-    .sidenav a:hover {
-        color: #f1f1f1;
-    }
-
-    /* Position and style the close button (top right corner) */
-    .sidenav .closebtn {
-        position: absolute;
-        top: 0;
-        right: 25px;
-        font-size: 36px;
-        margin-left: 50px;
-    }
-
-    /* Style page content - use this if you want to push the page content to the right when you open the side navigation */
-    #main {
-        transition: margin-left .5s;
-        padding: 20px;
-    }
-
-    /* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
-    @media screen and (max-height: 450px) {
-        .sidenav {padding-top: 15px;}
-        .sidenav a {font-size: 18px;}
-    } 
-    </style>
-
-    <script>
-        /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-    function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-    }
-
-    /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-    function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
-    }
-    </script>
+    <script src="cnd/js/loader.js"></script>
 </head>
 <body>
-    <div id="mySidenav" class="sidenav">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="index.php">Home</a>
-        <a href="pricing.html">Pricing</a>
-        <a href="about.html">About Us</a>
+    <div class="loader"></div>
+    <div class="container">
+        <h1>About us</h1>
     </div>
-      
-    
-      <!-- <span onclick="openNav()">open</span> -->
-      
-      
-    <div id="main">
-        <div class="container">
-            <h1>About us</h1>
-        </div>
-    </div> 
-    
 
-    
 </body>
 </html>
